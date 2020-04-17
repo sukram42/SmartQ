67be045148337e61d2ac5f23e9f7fee9530bbe81
papstchaka <alex.christoph.ac@gmail.com>
2020-04-17 21:19:43 +0200

    basic routing

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, json, request
 from flask_jwt import JWT, jwt_required
 from backend import *
 
@@ -7,31 +7,55 @@ app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'super-secret'
 
-jwt = JWT(app, authenticate, identity)
+# jwt = JWT(app, authenticate, identity)
 
+success = True
 
 @app.route('/register', methods=['GET', 'POST'])
 @jwt_required()
 def register():
-    pass
+    data=[1,2,3]
+    
+    if success:
+        return app.response_class(response=json.dumps(data),status=200,mimetype='application/json')
+    return app.response_class(response=json.dumps("Access not allowed"),status=401,mimetype='application/json')
 
 @app.route('/signin', methods=['GET', 'POST'])
 @jwt_required()
 def sign():
-    pass
+    data=[1,2,3]
+    
+    if success:
+        return app.response_class(response=json.dumps(data),status=200,mimetype='application/json')
+    return app.response_class(response=json.dumps("Access not allowed"),status=401,mimetype='application/json')
 
 @app.route('/<shop>/update', methods=['GET', 'POST'])
 @jwt_required()
-def update():
-    pass
+def update(shop):
+    data=[1,2,3]
+    
+    if success:
+        return app.response_class(response=json.dumps(data),status=200,mimetype='application/json')
+    return app.response_class(response=json.dumps("Access not allowed"),status=401,mimetype='application/json')
 
 @app.route('/<shop>', methods=['GET', 'POST'])
-def shopinfo():
-    pass
+def shopinfo(shop):
+    data=[1,2,3]
+    
+    if success:
+        return app.response_class(response=json.dumps(data),status=200,mimetype='application/json')
+    return app.response_class(response=json.dumps("Access not allowed"),status=401,mimetype='application/json')
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-    return render_template('index.html')
+    data=[1,2,3]
+    
+    if request.method == 'GET':
+        data=[2,3,4]
+    
+    if success:
+        return app.response_class(response=json.dumps(data),status=200,mimetype='application/json')
+    return app.response_class(response=json.dumps("Access not allowed"),status=401,mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True)
