440c5fa3164312ca42b379a58627d93b046cfd65
ge69kow <ge69kow@mytum.de>
2020-04-17 21:56:47 +0200

    started work on shopManaging

---
+++ frontend/src/views/ShopManage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class='shoppanel'>
+        You are logged in as __StoreOwnerX__
+       <div v-for="shop in shops" :key="shop.id">
+           <shopDetails v-bind:shop="shop" v-on:edit-shop="editmode"/>
+       </div>
+    </div>
+</template>
+
+<script>
+import shopDetails from './ShopManageDetail'
+export default {
+  name: 'ShopManage',
+  components: {
+    shopDetails
+  },
+  methods: {
+    editmode (id) {
+      console.log('Open edit page') //  TODO
+    }
+  },
+  props: ['shops']
+}
+</script>
+
+<style scoped>
+.shoppanel {
+    margin-left: 2%;
+    margin-right: 2%;
+    margin-top: 2%;
+    padding: 30px;
+    padding-bottom: 50px;
+    background: orchid;
+}
+</style>
---
+++ frontend/src/views/ShopManageDetail.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="shopHeader">
+        <h2>{{shop.name}}</h2>
+        <div class="shopDetails">
+            <center>
+            <table class="editList">
+                <tr>
+                    <th>Category</th><th><input type="text" v-model="shop.category" readonly="true"/> </th>
+                </tr><tr>
+                   <th>Address</th><th><input type="text" v-model="shop.address" readonly="true"/> </th>
+                </tr><tr>
+                  <th>Latitude</th><th><input type="text" v-model="shop.latitude" readonly="true"/> </th>
+                </tr><tr>
+                  <th>Longditude</th><th><input type="text" v-model="shop.longitude" readonly="true"/> </th>
+                </tr><tr>
+                  <th>Storespace</th><th><input type="text" v-model="shop.storespace" readonly="true"/> </th>
+                </tr><tr>
+                  <th>MaxCapacity</th><th><input type="text" v-model="shop.maxcapacity" readonly="true"/> </th>
+                </tr><tr>
+                  <th>CurrentCapacity</th><th><input type="text" v-model="shop.capacity" readonly="true"/> </th>
+                </tr><tr>
+                  <th>Waitingtime</th><th><input type="text" v-model="shop.waitingtime" readonly="true"/> </th>
+                </tr>
+            </table>
+            </center>
+        <button @click="$emit('edit-shop', shop, this)">Edit</button>
+        <button @click="$emit('remove-shop',shop.id)">Remove</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ShopDetails',
+  props: ['shop']
+}
+</script>
+
+<style scoped>
+.shopHeader{
+    background: orangered;
+    padding: 10px;
+    margin-top: 10px;
+    margin-left: 2%;
+    margin-right: 2%;
+
+}
+
+.shopDetails{
+    background: orange;
+    padding: 10px;
+}
+
+.editList, th , td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    margin: 5px;
+    padding: 5px;
+}
+
+</style>
