1eafa74b5d68b4e4181c5a8682a1356cb383757d
Markus Böbel <markus.boebel@tum.de>
2020-04-17 17:37:42 +0200

    Add header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,75 @@
 <template>
   <div id="app">
     <header id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/shop">Shop</router-link>
+      <div class="logo">Queueti</div>
+      <div class="links">
+        <router-link to="/">Home</router-link>
+        <router-link to="/shop">Shop</router-link>
+      </div>
     </header>
-    <router-view/>
+    <div class="content">
+      <router-view/>
+    </div>
+
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  $header_size: 50px;
+  $header_color: #000;
+  $header_text_color: #FFF;
 
-#nav {
-  padding: 30px;
+  html, body {
+    width: 100vw;
+    height: 100%;
+    max-height: 100%;
+    min-height: 100%;
+    margin: 0;
+  }
 
-  a {
-    font-weight: bold;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
+  }
 
+  .logo {
+    color: white;
+    font-size: 2em;
+    font-family: fantasy;
+    margin: 0 1em;
+  }
+
+  .content {
+    padding-top: $header_size;
+  }
+
+  #nav {
+    /*padding: 0 4%;*/
+    display: inline-flex;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    min-width: 100%;
+    height: $header_size;
+    background-color: $header_color;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .links > * {
+    margin: 0 1em;
+    color: $header_text_color;
+    text-decoration: none;
+    font-weight: lighter;
+  }
+  a{
     &.router-link-exact-active {
+      /*Style for active tab*/
       color: #42b983;
     }
   }
-}
 </style>
