3d6744f7b1c592f36f1b746e9c45c469821f6d3a
papstchaka <alex.christoph.ac@gmail.com>
2020-04-17 22:25:04 +0200

    added communication of endpoints

--- backend/backend.py
+++ backend/backend.py
@@ -1,30 +1,39 @@
 from werkzeug.security import safe_str_cmp
 
 
-
+def get_all_users():
+    users = [12] #load all users
+    username_table = {u.username: u for u in users}
+    userid_table = {u.id: u for u in users}
+    return username_table, userid_table
 
 def authenticate(username, password):
+    username_table, _ = get_all_users()
     user = username_table.get(username, None)
     if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
         return user
 
 def identity(payload):
+    _, userid_table = get_all_users()
     user_id = payload['identity']
     return userid_table.get(user_id, None)
 
 
 
-def update():
+def update_database(shop, id_, peoplechange):
     pass
 
-def register():
+def register(signin_data):
     pass
 
-def login():
-    pass
+def login(username, password):
+    return True
 
 def get_shops():
-    pass
+    return []
 
-def get_shopinfo():
+def get_shopinfo(shop, id_, time):
+    return "xoxo"
+
+def add_shop(shop, shopinfo):
     pass
--- backend/run.py
+++ backend/run.py
@@ -12,50 +12,59 @@ app.config['SECRET_KEY'] = 'super-secret'
 success = True
 
 @app.route('/register', methods=['GET', 'POST'])
-@jwt_required()
-def register():
-    data=[1,2,3]
+# @jwt_required()
+def register_user():
     
-    if success:
-        return app.response_class(response=json.dumps(data),status=200,mimetype='application/json')
-    return app.response_class(response=json.dumps("Access not allowed"),status=401,mimetype='application/json')
+    if request.method == "POST":
+        signin_data = request.json
+        register(signin_data)
+        
+        return app.response_class(response=json.dumps("User was added"),status=200,mimetype='application/json')
+    return app.response_class(response=json.dumps("Service unavailable"),status=503,mimetype='application/json')
 
 @app.route('/signin', methods=['GET', 'POST'])
-@jwt_required()
-def sign():
-    data=[1,2,3]
+# @jwt_required()
+def login_user():
     
-    if success:
-        return app.response_class(response=json.dumps(data),status=200,mimetype='application/json')
-    return app.response_class(response=json.dumps("Access not allowed"),status=401,mimetype='application/json')
+    if request.method == "POST":
+        username, password = request.json["login"], request.json["password"]
+        access_granted = login(username, password)
+        
+        if access_granted:
+            return app.response_class(response=json.dumps("Access granted"),status=200,mimetype='application/json')
+        return app.response_class(response=json.dumps("Access denied"),status=401,mimetype='application/json')
+    return app.response_class(response=json.dumps("Service unavailable"),status=503,mimetype='application/json')
 
 @app.route('/<shop>/update', methods=['GET', 'POST'])
-@jwt_required()
+# @jwt_required()
 def update(shop):
-    data=[1,2,3]
     
-    if success:
-        return app.response_class(response=json.dumps(data),status=200,mimetype='application/json')
-    return app.response_class(response=json.dumps("Access not allowed"),status=401,mimetype='application/json')
+    if request.method == "POST":
+        id_, peoplechange = request.json["id"], request.json["people_change"]
+        update_database(shop, id_, peoplechange)
+        return app.response_class(response=json.dumps(f"Updated {shop}\'s number of people"),status=200,mimetype='application/json')
+    return app.response_class(response=json.dumps("Service unavailable"),status=503,mimetype='application/json')
 
 @app.route('/<shop>', methods=['GET', 'POST'])
 def shopinfo(shop):
-    data=[1,2,3]
     
-    if success:
-        return app.response_class(response=json.dumps(data),status=200,mimetype='application/json')
-    return app.response_class(response=json.dumps("Access not allowed"),status=401,mimetype='application/json')
+    if request.method == "GET":
+        id_, time = request.json["id"], request.json["time"]
+        shopinfo = get_shopinfo(shop, id_, time)
+        return app.response_class(response=json.dumps(shopinfo),status=200,mimetype='application/json')
+    elif request.method == "POST":
+        shopinfo = request.json
+        add_shop(shop, shopinfo)
+        return app.response_class(response=json.dumps(f"{shop}\'s data was added in database"),status=200,mimetype='application/json')
+    return app.response_class(response=json.dumps("Service unavailable"),status=503,mimetype='application/json')
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-    data=[1,2,3]
     
-    if request.method == 'GET':
-        data=[2,3,4]
-    
-    if success:
-        return app.response_class(response=json.dumps(data),status=200,mimetype='application/json')
-    return app.response_class(response=json.dumps("Access not allowed"),status=401,mimetype='application/json')
+    if request.method == "GET":
+        shops = get_shops()
+        return app.response_class(response=json.dumps(shops),status=200,mimetype='application/json')
+    return app.response_class(response=json.dumps("Service unavailable"),status=503,mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True)
