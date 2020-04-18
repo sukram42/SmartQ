0d9184f43a272638788118d22ed217e5d397d69c
Markus Böbel <markus.boebel@tum.de>
2020-04-18 11:33:19 +0200

    Added Shop Detail Page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import ShopDetailPage from '../views/ShopDetailPage.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/shopDetail/:id',
+    name: 'ShopDetail',
+    component: ShopDetailPage
   },
   {
     path: '/shop',
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,23 +7,47 @@
       </div>
     </div>
     <div id="map" ref="map">
+      <maps-marker :lat="-25.344"
+                   :lng="131.036"
+                   :onClick="onMarkerClick"
+                   :shop="{name: 'Rewe', id: 'blabla'}"
+      />
     </div>
   </div>
 </template>
 <script>
 
 // @ is an alias to /src
+import MapsMarker from '../components/MapsMarker'
+
 export default {
   name: 'Home',
-  components: {},
-
+  components: { MapsMarker },
   data: () => ({
     map: null
   }),
+  methods: {
+    onMarkerClick (shop) {
+      this.$router.push({ path: `shopDetail/${shop.name}` })
+    },
+    getMap (callback) {
+      /* eslint-disable */
+        const that = this
+        /* eslint-enable */
+      function checkForMap () {
+        if (that.map) {
+          callback(that.map)
+        } else {
+          setTimeout(checkForMap, 200)
+        }
+      }
+      checkForMap()
+    }
 
+  },
   mounted () {
     this.map = new window.google.maps.Map(this.$refs.map, {
-      center: { lat: -34.397, lng: 150.644 },
+      center: { lat: -25.344, lng: 131.036 },
       zoom: 8,
       streetViewControl: false,
       fullscreenControl: false,
@@ -33,13 +57,14 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-  .search_input_wrapper{
+  .search_input_wrapper {
     position: absolute;
     width: 100vw;
     z-index: 100;
     padding: 1em;
   }
-  .search_input{
+
+  .search_input {
     border-radius: 5px;
     z-index: 1000;
     background-color: white;
@@ -47,6 +72,7 @@ export default {
     padding: 1% 4% 0 4%;
 
   }
+
   #map {
     height: calc(100vh - 50px);
     width: 100vw;
