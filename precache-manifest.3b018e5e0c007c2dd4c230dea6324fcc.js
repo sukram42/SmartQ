eb9b5fc49857c41a881e6dc671ae3ede47eb35bc
ge69kow <ge69kow@mytum.de>
2020-04-19 03:51:41 +0200

    first Version

---
+++ precache-manifest.3b018e5e0c007c2dd4c230dea6324fcc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f57bed6adc31b27330",
+    "url": "/hackatum-corona/css/about.47dd1ed6.css"
+  },
+  {
+    "revision": "f420da23b6135d4d4e42",
+    "url": "/hackatum-corona/css/app.e95a8f0f.css"
+  },
+  {
+    "revision": "98cc5203ff197604e257",
+    "url": "/hackatum-corona/css/chunk-vendors.c4a1338c.css"
+  },
+  {
+    "revision": "a56b9f8bf8b88b88d6365944cf28c191",
+    "url": "/hackatum-corona/img/bernd-bangert-ZUbfL_httr4-unsplash.a56b9f8b.jpg"
+  },
+  {
+    "revision": "ecc2b5cf536233b4504834effeec183f",
+    "url": "/hackatum-corona/img/hanson-lu-sq5P00L7lXc-unsplash.ecc2b5cf.jpg"
+  },
+  {
+    "revision": "9d484dde70251a9fbd5b19ec2a16aad2",
+    "url": "/hackatum-corona/img/jelleke-vanooteghem-MohB4LCIPyM-unsplash.9d484dde.jpg"
+  },
+  {
+    "revision": "b30e25ed3a0e42680e1c1218d9b9f1d3",
+    "url": "/hackatum-corona/img/patrick-tomasso-GXXYkSwndP4-unsplash.b30e25ed.jpg"
+  },
+  {
+    "revision": "dbdb522b12a240e0a6012644e8550cfe",
+    "url": "/hackatum-corona/index.html"
+  },
+  {
+    "revision": "27f57bed6adc31b27330",
+    "url": "/hackatum-corona/js/about.bcc3b410.js"
+  },
+  {
+    "revision": "f420da23b6135d4d4e42",
+    "url": "/hackatum-corona/js/app.2d8a7dcb.js"
+  },
+  {
+    "revision": "98cc5203ff197604e257",
+    "url": "/hackatum-corona/js/chunk-vendors.d8fcd8ae.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/hackatum-corona/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hackatum-corona/robots.txt"
+  }
+]);
